02720c0ca6b34cb1d85112353e8fae98e92dd0f3
mayol_qo0tzvv <mayol.picard@gmx.fr>
2026-09-11 12:40:04 +0200

    V2.6

--- capitalisme_poo.py
+++ capitalisme_poo.py
@@ -7,9 +7,10 @@ import pygame
 from print_in_screen import *
 hauteur_ecran=600
 longueur_ecran=1200
-position_bouton_menu=-1
 pseudo="test"
 file = "sauvegard_" + pseudo + ".pkl"
+position_bouton_menu=1
+
 class Joueur ():
   def __init__(self,save):
     self.carburantT=save["carburantT"]
@@ -19,7 +20,6 @@ class Joueur ():
     self.Namorti=save["Namorti"]
 class Start:
   def __init__(self, pseudo):
-    self.running = True
     self.pseudo=pseudo
     self.running = True
     self.clock = pygame.time.Clock()
@@ -28,18 +28,19 @@ class Start:
     save_game(file, self.joueur.carburantT, self.joueur.argent, self.joueur.Ncarburant, self.joueur.amorti, self.joueur.Namorti)
 class Menu(Start):
   def __init__(self):
-    print(self.running)
-    self.display_menu()
+    super().__init__(self)
+    self.position_bouton_menu = position_bouton_menu
   def display_menu(self):
     self.screen = pygame.display.set_mode((longueur_ecran, hauteur_ecran))
     self.screen.fill("black")
     pygame.mouse.set_visible(False)
     pygame.draw.rect(self.screen,"red", pygame.Rect(0,hauteur_ecran//2-20,longueur_ecran, 40))
-    print_in_screen(self.screen,"jouer",[longueur_ecran//2-15,hauteur_ecran//2-18-((position_bouton_menu)*50)],size=50)
-    print_in_screen(self.screen, "stop", [longueur_ecran // 2 - 15, hauteur_ecran // 2 - 18-((1+position_bouton_menu)*50)], size=50)
-    print_in_screen(self.screen, "magasin", [longueur_ecran // 2 - 15, hauteur_ecran // 2 - 18-((-1+position_bouton_menu)*50)], size=50)
+    print_in_screen(self.screen,"jouer",[longueur_ecran//2-15,hauteur_ecran//2-18-((self.position_bouton_menu)*50)],size=50)
+    print_in_screen(self.screen, "stop", [longueur_ecran // 2 - 15, hauteur_ecran // 2 - 18-((1+self.position_bouton_menu)*50)], size=50)
+    print_in_screen(self.screen, "magasin", [longueur_ecran // 2 - 15, hauteur_ecran // 2 - 18-((-1+self.position_bouton_menu)*50)], size=50)
     pygame.display.flip()
-  def handling_events(self):
+
+  def handling_events_menu(self):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         self.running = False
@@ -47,14 +48,23 @@ class Menu(Start):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
-      self.player.velocity[1] = -1
+      self.position_bouton_menu+=1
+      self.display_menu()
+      sleep(0.1)
     elif keys[pygame.K_DOWN]:
-      self.player.velocity[1] = 1
-    else:
-      self.player.velocity[1] = 0
+      self.position_bouton_menu-=1
+      self.display_menu()
+      sleep(0.1)
+class Logique (Menu):
+  def __init__(self):
+    super().__init__()
+    self.run()
+  def run(self):
+    self.display_menu()
+    while self.running:
+      self.handling_events_menu()
+      self.clock.tick(60)
+
 
 pygame.init()
-game = Start(pseudo)
-game = Menu()
-while True:
-  a=1
+game = Logique()
