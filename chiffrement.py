530ac6ec6be0310a2bfd95fa48174640b60ac569
mayol_qo0tzvv <mayol.picard@gmx.fr>
2026-09-18 12:18:58 +0200

    chifrement

--- chiffrement.py
+++ chiffrement.py
@@ -5,18 +5,24 @@ def chiffrement(key):
   a=0
   while not a==b:
     a+=1
-    c=random.randint(0,1000)
+    c=random.randint(0,1000000)
   random.seed(c)
   b = random.randint(10, 100)
   a = 0
   while not a == b:
     a += 1
-    c = random.randint(0, 1000)
+    c = random.randint(0, 1000000)
   random.seed(c)
   b = random.randint(10, 100)
   a = 0
   while not a == b:
     a += 1
-    c = random.randint(0, 1000)
+    c = random.randint(0, 10000000)
   print(c)
-chiffrement(2)
+def chiffrement_mot_de_passe(mot_de_passe):
+  convertion=""
+  for i in mot_de_passe:
+    convertion+=str(ord(i))
+  convertion=int(convertion)
+  chiffrement(convertion)
+chiffrement_mot_de_passe("Mayol")
--- save_op.py
+++ save_op.py
@@ -1,7 +1,6 @@
 import pickle
 def save_game(file, carburantT=10, argent=0, Ncarburant=1, amorti=1, Namorti=1,hauteur_ecran=600,longueur_ecran=1200):
   save={"carburantT":carburantT,"argent":argent,"Ncarburant":Ncarburant,"amorti":amorti,"Namorti":Namorti,"hauteur_ecran":hauteur_ecran,"longueur_ecran":longueur_ecran}
-  print(save)
   with open(file, 'wb') as f:
     pickle.dump(save, f)
 def load(file):
@@ -13,3 +12,26 @@ def load(file):
   save = pickle.load(f)
   f.close()
   return save
+def utilisateur(nom,mot_de_passe_chiffré):
+  try:
+    f = open("utilisateur.pkl", "rb")
+  except FileNotFoundError:
+    with open("utilisateur.pkl", 'wb') as f:
+      pickle.dump({}, f)
+    return [False,False]
+  liste_utilisateur = pickle.load(f)
+  if not liste_utilisateur.get(nom,"cet uttilisateur n'existe pas")=="cet uttilisateur n'existe pas":
+    if liste_utilisateur[nom]==mot_de_passe_chiffré:
+      return [True,True]
+    else:
+      return [True,False]
+  else:
+    return [False,False]
+  f.close
+def add_utilisateur(nom,mot_de_passe_chiffré):
+  f = open("utilisateur.pkl", "rb")
+  liste_utilisateur = pickle.load(f)
+  f.close
+  liste_utilisateur[nom] = mot_de_passe_chiffré
+  with open("utilisateur.pkl", 'wb') as f:
+    pickle.dump(liste_utilisateur, f)
